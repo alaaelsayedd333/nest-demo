a415dc5742bb045257c52a19b2e0fa4cbd4cb779
alaa <alaa@alaa.com>
2026-06-22 22:35:24 +0300

    fix mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './schema/user.schema';
+
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal:true}),MongooseModule],
+  imports: [ConfigModule.forRoot({isGlobal:true, }),  MongooseModule.forFeature([
+      { name: 'User', schema: UserSchema }
+    ]), MongooseModule.forRoot(process.env.MONGO_URI!)],
   controllers: [AppController],
   providers: [AppService],
 })
